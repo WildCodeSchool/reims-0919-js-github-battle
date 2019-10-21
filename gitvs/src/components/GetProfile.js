da22a52c7d696285023fbc77e043f9dc56d38b52
Aloxbro <alox51@wanadoo.fr>
2019-10-21 17:45:11 +0200

    add css + some styling

--- gitvs/src/components/GetProfile.js
+++ gitvs/src/components/GetProfile.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import './getProfile.css'
 
 function GetProfile({name, avatar_url, public_repo, location}) {
     return (
-        <div>
+        <div className='card'>
             <h1 className='githubNickname'>{name}</h1>
-            <img className='avatarLogo' src={avatar_url} alt='avatar'></img>
-            <p className='userInfo'>
-            {location}
-            {public_repo}
+                <div className='avatarBox'>
+                    <img className='avatar' src={avatar_url} alt='avatar'></img>
+                </div>
+            <p className='location'>
+                Location: {location} 
+            <br />
+            </p> 
+                <p className='repo'>
+            Public Repositories: {public_repo}
+            <br />
             </p>
         </div>
     )
