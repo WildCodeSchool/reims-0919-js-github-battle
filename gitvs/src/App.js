1e9ef64b1e4df91088bb9a7f9f79cd63632b12b0
Aloxbro <alox51@wanadoo.fr>
2019-10-24 14:48:01 +0200

    Fixed PR review + Eslint Airbnb rule applied

--- gitvs/src/App.js
+++ gitvs/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import './App.css';
-import GetProfile from './components/GetProfile';
+/* eslint-disable import/extensions */
+import React from 'react'
+import './App.css'
+import GetProfile from './components/GetProfile.jsx'
 
 function App() {
   return (
     <div className="App">
-      <GetProfile name='Alox' 
-      avatar_url='https://avatars3.githubusercontent.com/u/8034782?s=460&v=4' 
-      public_repo='6' 
-      location='Reims'
-      weapons='JavaScript'/>
+      <GetProfile
+        name="Alox"
+        avatarUrl="https://avatars3.githubusercontent.com/u/8034782?s=460&v=4"
+        publicRepo="6"
+        location="Reims"
+        weapons="JavaScript"
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
