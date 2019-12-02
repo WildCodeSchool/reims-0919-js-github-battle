63324b152ff88f0566e5889b5143ae08977efed6
RG <robin.guerre@yahoo.fr>
2019-12-02 17:21:09 +0100

    Update ComparisonOfTwoProfiles.js

--- gitvs/src/components/ComparisonOfTwoProfiles.js
+++ gitvs/src/components/ComparisonOfTwoProfiles.js
@@ -42,28 +42,6 @@ function ComparisonOfTwoProfiles(props) {
             secondUserPublic_Gists={props.location.state.secondUserPublic_Gists}
           />
         </div>
-        {/* <DisplayChosenProfile1
-          firstAvatar_Url={props.location.state.firstAvatar_Url}
-          firstUsername={props.location.state.firstUsername}
-          firstUserLocation={props.location.state.firstUserLocation}
-          firstUserLanguage={props.location.state.firstUserLanguage}
-        />
-        <Comparison
-          firstAvatar_Url={props.location.state.firstAvatar_Url}
-          secondAvatar_Url={props.location.state.secondAvatar_Url}
-          firstUserRepos={props.location.state.firstUserRepos}
-          secondUserRepos={props.location.state.secondUserRepos}
-          firstUserFollowers={props.location.state.firstUserFollowers}
-          secondUserFollowers={props.location.state.secondUserFollowers}
-          firstUserPublic_Gists={props.location.state.firstUserPublic_Gists}
-          secondUserPublic_Gists={props.location.state.secondUserPublic_Gists}
-        />
-        <DisplayChosenProfile2
-          secondAvatar_Url={props.location.state.secondAvatar_Url}
-          secondUsername={props.location.state.secondUsername}
-          secondUserLocation={props.location.state.secondUserLocation}
-          secondUserLanguage={props.location.state.secondUserLanguage}
-        /> */}
       </div>
       <button type="button"><Link to="/home">New Fight</Link></button>
     </div>
